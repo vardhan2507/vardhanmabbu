87034116be476a97aca7cb4c85e851c81bea661a
vardhan250798 <vardhan@inncircles.com>
2020-06-06 23:05:21 +0530

    changed env urls for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { Environment } from './environment-variables';
 
 export const environment = {
   production: true,
-  apiUrl: Environment.apiUrl,
-  ioSocketUrl: Environment.ioSocketUrl,
+  apiUrl: process.env.apiUrl || 'https://vardhanmabbuapp.herokuapp.com',
+  ioSocketUrl: process.env.socketUrl || 'https://vardhanmabbuapp.herokuapp.com:2507',
 };
